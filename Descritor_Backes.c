1d4399d3bc89d925fe5960ab3310cdf89a45aa08
Mariana Dias <marianaapdias96@gmail.com>
2016-07-26 16:37:00 -0300

    Funcao que le as coordenadas adicionada

--- Descritor_Backes.c
+++ Descritor_Backes.c
@@ -2,7 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+float pontosX[10];
+float pontosY[10];
+
+void Coordenadas()
+{
+  int i;
+  printf("Insira as coordenadas do contorno da forma 2D:\n");
+  for(i=0;i<10;i++)
+  {
+      printf("x %d: ",i);
+      scanf("%d",&pontosX[i]);
+      printf("y %d: ",i);
+      scanf("%d",&pontosY[i]);
+  }
+}
+
 int main()
 {
+  Coordenadas();
   return 0;
 }
