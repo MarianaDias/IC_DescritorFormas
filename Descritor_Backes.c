799dc9aac0a84adad4961c84bfeb1cb6d13d406b
Mariana Dias <marianaapdias96@gmail.com>
2016-07-26 17:20:34 -0300

    Função de calculo da distancia euclidiana

--- Descritor_Backes.c
+++ Descritor_Backes.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <math.h>
 
 float pontosX[10];
 float pontosY[10];
+float Arestas[10][10];
 
 void Coordenadas()
 {
@@ -12,14 +13,31 @@ void Coordenadas()
   for(i=0;i<10;i++)
   {
       printf("x %d: ",i);
-      scanf("%d",&pontosX[i]);
+      scanf("%f",&pontosX[i]);
       printf("y %d: ",i);
-      scanf("%d",&pontosY[i]);
+      scanf("%f",&pontosY[i]);
   }
+}
+
+void DistanciaEuclidiana()
+{
+  int i,j;
+  float d;
+  for(i=0;i< 10;i++)
+  {
+    for(j=i+1;j< 10; j++)
+    {
+      d = pow((pontosX[i] - pontosX[j]),2) + pow((pontosY[i] - pontosY[j]),2);
+      Arestas[i][j] = sqrt(d);
+      printf("[%d,%d] = %.2f\n",i,j,Arestas[i][j]);
+    }
+  }
+  printf("\n-------- Calculou ------ \n");
 }
 
 int main()
 {
   Coordenadas();
+  DistanciaEuclidiana();
   return 0;
 }
